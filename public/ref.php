431977caf6105ed7c8c414a8817276e1541bb3a6
bigguu <49320396+bigguu@users.noreply.github.com>
2019-04-29 22:17:40 +0700

    undone main,login & regis

---
+++ public/ref.php
@@ -0,0 +1,33 @@
+﻿<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>auth</title>
+    <script src="https://www.gstatic.com/firebasejs/5.10.1/firebase.js"></script>
+    <script>
+        var config = {
+            apiKey: "AIzaSyCRL8xcJoK7jj3r6s7OtdCiZz5djGuKjyo",
+            authDomain: "check-name-c3dae.firebaseapp.com",
+            databaseURL: "https://check-name-c3dae.firebaseio.com",
+            projectId: "check-name-c3dae",
+            storageBucket: "check-name-c3dae.appspot.com",
+            messagingSenderId: "279764457881"
+        };
+        firebase.initializeApp(config);
+        var provider = new firebase.auth.GoogleAuthProvider();
+        function onSignInButtonClick() {
+            firebase.auth().signInWithPopup(provider).then(function (result) {
+                console.log(result);
+                //Do something when login complete
+				document.write("pass");
+            }).catch(function (error) {
+               document.write("error");
+			   //Do something when error
+            });
+        }
+    </script>
+</head>
+<body>
+    <button onclick="onSignInButtonClick()">Login</button>
+</body>
+</html>
