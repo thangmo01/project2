a752d07380969e0a4da13a0d5679f63148892ef7
bigguu <49320396+bigguu@users.noreply.github.com>
2019-04-30 21:31:48 +0700

    commit message

---
+++ app/views/common/time.php
@@ -0,0 +1,5 @@
+<?php date_default_timezone_set("Asia/Bangkok");
+			echo "Today is " . date("Y/m/d") . "<br>";
+			echo "Now it is " . date("H:i:s") . "<br>";
+			echo "Today is " . date("l");
+		?>
--- app/views/tests/index.php
+++ app/views/tests/index.php
@@ -1,3 +1,3 @@
 <?php require APPROOT . '/views/common/header.php'; ?>
-<h1>Test</h1>
+<?php require APPROOT . '/views/common/time.php'; ?>
 <?php require APPROOT . '/views/common/footer.php'; ?>
