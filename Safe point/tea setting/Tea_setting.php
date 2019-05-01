5902249ed9c050d176059f855365dc665a81ddb0
PK9856 <49333482+PK9856@users.noreply.github.com>
2019-05-02 01:43:02 +0700

    Update Tea_setting.php

--- Safe point/tea setting/Tea_setting.php
+++ Safe point/tea setting/Tea_setting.php
@@ -1,5 +1,4 @@
-﻿<!DOCTYPE html>
-
+<!DOCTYPE html>
 <html lang="en" xmlns="http://www.w3.org/1999/xhtml">
 <head>
     <meta charset="utf-8" />
@@ -9,93 +8,114 @@
     <link rel="stylesheet" type="text/css" media="screen" href="./main.css" />
 </head>
 <body>
-
     <div id="mySidebar" class="sidebar">
         <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">×</a>
         <img class="img" src="team2.jpg" alt="John" style="width:50%">
-       <br> <input type="text" name="firstname" value="enter your class">
-                <a href="#">main</a>
-                <a href="#">history</a>
-                <a href="#">setting</a>
-                <a href="#">logout</a>
-                
+        <br><input type="text" name="firstname" value="enter your class">
+            <a href="#">main</a>
+            <a href="#">history</a>
+            <a href="#">setting</a>
+            <a href="#">logout</a>
     </div>
 
     <div id="main">
         <button class="openbtn" onclick="openNav()">☰ Option</button>
    
     
-<?php       /* time
-    echo"<br><br><br><br>";
-    echo date("d/m/Y")."<br>";
-    date_default_timezone_set("Asia/Bangkok");
-    date("H:i:s")."<br>";   */
+<?php //mysql
+
+//connect
+    $objConnect = mysqli_connect("localhost","root","","proj") or die("Error Connect to Database");
+        
+        
+//stu (fname , lname)
+    $user = "SELECT * FROM users WHERE user_type_id =2";
+    $student = mysqli_query($objConnect,$user);
+
+//stuID
+    $userstu = "SELECT * FROM user_students";
+    $userstuID = mysqli_query($objConnect,$userstu);
+
+//sec
+    $class = "SELECT * FROM classes";
+    $section = mysqli_query($objConnect,$class);
+    $result3 = mysqli_fetch_array($section);// must be 2
+    $sec = $result3['section'];   
 ?>    
     
-<div id="clock"></div>
+    <div id="clock"></div>
 
 <?php    /*table*/
-        echo"<table align = left>";
-        $column = 1;
-        $row = 0;
 
-        echo"<tr>";
-        echo"<th width=18%>Student ID</th>
-          <th width=50%>Name</th>
-          <th width=12%>Group</th>
-          <th width=20%>Remove</th>";
+    echo"<table align = left>";
+        echo"<tr>"; //head table
+            echo "<th width=18%>Student ID</th>
+            <th width=50%>Name</th>
+            <th width=12%>Group</th>
+            <th width=20%>Remove</th>";
         echo"</tr>";
-        
-        for ($column=1; $column <= 10; $column++) 
-        {   echo"<tr align = center>";    /*data in table*/
-            echo"<td>60010640</td>
-            <td align=left>Pongsathorn Kaosamphan</td>
-            <td>1</td>
-            <td><button>remove</button></td>";
-           
+    
+    //loop recieve data from sql to table    
+        while($result1 = mysqli_fetch_array($student))
+        {   //data in table
+            //name
+                $fname = $result1['first_name'];
+                $lname = $result1['last_name'];
+            //student id
+            $result2 = mysqli_fetch_array($userstuID);
+            $stuID = $result2['student_id'];
+                
+            echo"<tr align = center>";  //data  
+                echo"<td>$stuID</td>
+                <td align=left>$fname $lname</td>
+                <td>$sec</td>
+                <td><button onclick=location.href=''>remove</button></td>";
             echo"</tr>";
             
         }
-        echo"</table>";
+    echo"</table>";
+
 ?>
 
-    <script>
-        function openNav() {
+<?php   //close mysql
+     mysqli_close($objConnect);
+?>
+
+<script>    // Sidebar big
+        function openNav() 
+        {
             document.getElementById("mySidebar").style.width = "300px";
             document.getElementById("main").style.marginLeft = "300px";
         }
 
-        function closeNav() {
+        function closeNav() 
+        {
             document.getElementById("mySidebar").style.width = "0";
             document.getElementById("main").style.marginLeft = "0";
         }
-    </script>
+</script>
 
 <script>        /* clock */
     function showTime() 
     {
-    var date = new Date();
-    var hr = date.getHours();
-    var min = date.getMinutes();
-    var sec = date.getSeconds();
+        var date = new Date();
+        var hr = date.getHours();
+        var min = date.getMinutes();
+        var sec = date.getSeconds();
     
-    hr = (hr === 0) ? 24 : hr;
-    if (hr > 23) 
-    {
-        hr = hr - 24;
-        
-    }
-    hr = (hr < 10) ? "0" + hr : hr;
-    min = (min < 10) ? "0" + min : min;
-    sec = (sec < 10) ? "0" + sec : sec;
-  
-    var time = hr + ":" + min + ":" + sec + " ";
-    var clock = document.querySelector('#clock');
-    clock.innerText = time;
-    setTimeout(showTime, 1000);
+        hr = (hr === 0) ? 24 : hr;
+        if (hr > 23) 
+            {   hr = hr - 24;   }
+        hr = (hr < 10) ? "0" + hr : hr;
+        min = (min < 10) ? "0" + min : min;
+        sec = (sec < 10) ? "0" + sec : sec;
+        var time = hr + ":" + min + ":" + sec + " ";
+        var clock = document.querySelector('#clock');
+        clock.innerText = time;
+        setTimeout(showTime, 1000);
     }
     showTime();
 </script>
-</div>     
+    </div>     
 </body>
 </html>
