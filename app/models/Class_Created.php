e1d26959946d98e5db5176914a2d48c33863517c
Failmaker <49320398+Failmaker@users.noreply.github.com>
2019-05-01 23:24:56 +0700

    a

--- app/models/Class_Created.php
+++ app/models/Class_Created.php
@@ -9,7 +9,7 @@
         {
             $this->db->query('SELECT id FROM subjects WHERE name = :subject_name');
             $this->db->bind(':subject_name',$subject_name);
-            $subject_id=$this->db->fetchAll();;
+            $subject_id=$this->db->fetchAll();
             $this->db->query('SELECT * FROM semasters WHERE academic_year = :year and semaster = :semester');
             $this->db->bind(':academic_year',$year);
             $this->db->bind(':semester',$semester);
