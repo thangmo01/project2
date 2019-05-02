1f512b9d0b02072025464c4831c348701c5a8658
60010890 <60010890@kmitl.ac.th>
2019-05-02 09:08:16 +0700

    login

--- app/helpers/session_helper.php
+++ app/helpers/session_helper.php
@@ -2,3 +2,11 @@
     if(!session_id()){
         session_start();
     }
+
+    define('google_access_token', 'google_access_token');
+    define('user_id', 'user_id');
+    define('user_email', 'user_email');
+    define('user_name', 'user_name');
+    define('user_type', 'user_type');
+
+    define('student_id', 'student_id');
--- app/helpers/user_helper.php
+++ app/helpers/user_helper.php
@@ -1,8 +1,27 @@
 <?php
     function isLoggedIn(){
-        if(isset($_SESSION['user_id'])){
+        if(isset($_SESSION[user_id])){
         return true;
         } else {
         return false;
+        }
+    }
+
+    function checkLoggedIn() {
+        if(isLoggedIn()) {
+            switch ($_SESSION[user_type]) {
+                case 'student':
+                    redirect('student/index');
+                    break;
+                case 'teacher':
+                    redirect('teacher/index');
+                    break;
+            }
+        }
+    }
+
+    function createUserSession($data = []) {
+        foreach ($data as $key => $value) {
+            $_SESSION[$key] = $value;
         }
     }
