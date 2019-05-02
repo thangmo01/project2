c827468f74ab2386ab7a397e2533dc863fd35a6d
bigguu <49320396+bigguu@users.noreply.github.com>
2019-05-02 20:57:59 +0700

    Thangmo's css import (1pm)

--- app/views/common/header.php
+++ app/views/common/header.php
@@ -5,7 +5,9 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-
+    <!-- style -->
+    <link rel="stylesheet" href="<?php echo URLROOT . '/public/css/main.css';?>">
+    <!-- script -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/webcamjs/1.0.25/webcam.min.js"></script>
     
