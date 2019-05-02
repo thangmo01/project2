45c08dab20a3ca0b144c000d30bd92045704bf37
60010890 <60010890@kmitl.ac.th>
2019-05-02 20:56:16 +0700

    teacher'

--- app/controllers/Teachers.php
+++ app/controllers/Teachers.php
@@ -1,10 +1,33 @@
 <?php
     class Teachers extends Controller {
         public function __construct() {
-            checkLoggedIn('teacher');
+            // checkLoggedIn('teacher');
         }
         
         public function index() {
-            $this->view('teacher/index');
+            $data = [
+                '1' => [
+                    'subject_name' => 'OS',
+                    'subject_code' => '1',
+                    'student_count' => 40,
+                    'section' => 2,
+                    'class_key' => 'asdfkjdsa11fjklds'
+                ],
+                '2' => [
+                    'subject_name' => 'Data com',
+                    'subject_code' => '2',
+                    'student_count' => 39,
+                    'section' => 1,
+                    'class_key' => 'asdfkjdsaf5664jklds'
+                ],
+                '3' => [
+                    'subject_name' => 'Telecom',
+                    'subject_code' => '1',
+                    'student_count' => 41,
+                    'section' => 1,
+                    'class_key' => 'asdfkjdsafsadfjklds'
+                ]
+            ];
+            $this->view('teacher/index', $data);
         }
     }
--- app/views/teacher/index.php
+++ app/views/teacher/index.php
@@ -1,3 +1,9 @@
 <?php require APPROOT . '/views/common/header.php'; ?>
     <h1>teacher</h1>
+    <?php echo $data['title'] ?>
+    <?php
+        foreach ($data as $sub) {
+            print_r($sub);echo '<br>';
+        }
+    ?>
 <?php require APPROOT . '/views/common/footer.php'; ?>
