629288eebaf11e38ef53054bbb6066c0ad8aae51
bigguu <49320396+bigguu@users.noreply.github.com>
2019-05-03 23:40:03 +0700

    nav clock inside

--- app/views/teacher/class_detail.php
+++ app/views/teacher/class_detail.php
@@ -1,10 +1,12 @@
 <?php require APPROOT . '/views/common/header.php'; ?><div id="clock"></div></ul>
-        <h1><center>
+<div style="padding:20px; margin-top:30px;">   
+<h1>teacher</h1>
+</div>        
+<h1><center>
         <?php   
             echo "Class : ".$data['class_id']."<br>";        
         ?>
         </center></h1>
-    <div id="clock"></div>
 
 <?php    /*table*/
     echo"<table>";
