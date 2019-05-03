e06a2b552784125865365ee8eab894cb5ea3310a
bigguu <49320396+bigguu@users.noreply.github.com>
2019-05-03 22:46:07 +0700

    Nav

--- app/views/teacher/index.php
+++ app/views/teacher/index.php
@@ -1,5 +1,7 @@
 <?php require APPROOT . '/views/common/header.php'; ?></ul>
-    <h1>teacher</h1>
+<div style="padding:20px; margin-top:30px;">   
+<h1>teacher</h1>
+</div>
 <!--    <?php
         foreach ($data as $sub) {
             // print_r($sub);echo '<br>';
