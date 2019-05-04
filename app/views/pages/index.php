afb02f8b6634eee3c3f1c8696cd5bceba4c74f3d
bigguu <49320396+bigguu@users.noreply.github.com>
2019-05-04 23:59:17 +0700

    loginindexchange

--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -2,4 +2,24 @@
 <div>
     <?php echo isset($data['email_error']) ? '<h2 style="color: red">' .$data['email_error'] . '<h2>' : '';?>
 </div>
+<style>
+body, html {
+  height: 100%;
+  margin: 0;
+}
+</style>
+<div class="coverbgimg">
+  <div class="covermiddle">
+    <h1>Lorem Ipsum</h1>
+    <hr>
+    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+         when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+          It has survived not only five centuries, but also the leap into electronic typesetting,
+           remaining essentially unchanged. 
+           It was popularised in the 1960s with the release of Letraset sheets containing 
+           Lorem Ipsum passages, and more recently with desktop publishing software like 
+           Aldus PageMaker including versions of Lorem Ipsum.</p>
+  </div>
+</div>
 <?php require APPROOT . '/views/common/footer.php'; ?>
--- app/views/student/index.php
+++ app/views/student/index.php
@@ -4,6 +4,32 @@
 <div style="padding:20px; margin-top:30px;">
 <h1>student</h1>
 </div>
+<div style="padding:20px; margin-top:30px;">
+  <h1>Fixed Top Navigation Bar</h1>
+  <h2>Scroll this page to see the effect</h2>
+  <h2>The navigation bar will stay at the top of the page while scrolling</h2>
+
+  <p>Some text some text some text some text..</p>
+  <p>Some text some text some text some text..</p>
+  <p>Some text some text some text some text..</p>
+  <p>Some text some text some text some text..</p>
+  <p>Some text some text some text some text..</p>
+  <p>Some text some text some text some text..</p>
+  <p>Some text some text some text some text..</p>
+  <p>Some text some text some text some text..</p>
+  <p>Some text some text some text some text..</p>
+  <p>Some text some text some text some text..</p>
+  <p>Some text some text some text some text..</p>
+  <p>Some text some text some text some text..</p>
+  <p>Some text some text some text some text..</p>
+  <p>Some text some text some text some text..</p>
+  <p>Some text some text some text some text..</p>
+  <p>Some text some text some text some text..</p>
+  <p>Some text some text some text some text..</p>
+  <p>Some text some text some text some text..</p>
+  <p>Some text some text some text some text..</p>
+  <p>Some text some text some text some text..</p>
+</div>
 <script>        /* clock */
     function showTime() 
     {
