107ef771c964a619bd56d74f0a59b96f0d596952
bigguu <49320396+bigguu@users.noreply.github.com>
2019-05-05 21:09:24 +0700

    responcard for subject

--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -10,16 +10,12 @@ body, html {
 </style>
 <div class="coverbgimg">
   <div class="covermiddle">
-    <h1>Lorem Ipsum</h1>
+    <h1>Project2</h1>
     <hr>
-    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-         when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-          It has survived not only five centuries, but also the leap into electronic typesetting,
-           remaining essentially unchanged. 
-           It was popularised in the 1960s with the release of Letraset sheets containing 
-           Lorem Ipsum passages, and more recently with desktop publishing software like 
-           Aldus PageMaker including versions of Lorem Ipsum.</p>
+    <p>Project2 is our web project <br>that we made in this year 2019.<br></p>
+    <hr>
+    <h1><strong> Why it have to be project2?</strong></h2>
+    <p>Cause Project1 doesn't have "INOVATION"<br>So it vanished like Thanos snap his finger to it.</p>
   </div>
 </div>
 <?php require APPROOT . '/views/common/footer.php'; ?>
--- app/views/student/index.php
+++ app/views/student/index.php
@@ -1,3 +1,102 @@
 <?php require APPROOT . '/views/common/header.php'; ?>
 
+<style>
+* {
+  box-sizing: border-box;
+}
+
+body {
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+/* Float four columns side by side */
+.column {
+  float: left;
+  width: 25%;
+  padding: 0 10px;
+}
+
+/* Remove extra left and right margins, due to padding */
+.row {
+margin: 0 -5px;
+padding: 0 5px;
+}
+
+/* Clear floats after the columns */
+.row:after {
+  content: "";
+  display: table;
+  clear: both;
+  margin-bottom: 20px;
+}
+
+/* Responsive columns */
+@media screen and (max-width: 600px) {
+  .column {
+    width: 100%;
+    display: block;
+    margin-bottom: 20px;
+  }
+}
+
+/* Style the counter cards */
+.card {
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  padding: 16px;
+  text-align: center;
+  background-color: #f1f1f1;
+}
+</style>
+
+<div class="row">
+  <div class="column">
+    <div class="card">
+      <h3>Operate System</h3>
+      <p>example</p>
+      <p>Link</p>
+    </div>
+  </div>
+
+  <div class="column">
+    <div class="card">
+      <h3>Telecom</h3>
+      <p>154224518</p>
+      <p>Enter class</p>
+    </div>
+  </div>
+</div>
+
+<div class="row">
+  <div class="column">
+    <div class="card">
+      <h3>FoundationEng</h3>
+      <p>Some text</p>
+      <p>Some text</p>
+    </div>
+  </div>
+
+  <div class="column">
+    <div class="card">
+      <h3>SAD</h3>
+      <p>Some text</p>
+      <p>Some text</p>
+    </div>
+  </div>
+  
+  <div class="column">
+    <div class="card">
+      <h3>Webtech</h3>
+      <p>Some text</p>
+      <p>Some text</p>
+    </div>
+  </div>
+  
+  <div class="column">
+    <div class="card">
+      <h3>Datacom</h3>
+      <p>Some text</p>
+      <p>Some text</p>
+    </div>
+  </div>
+</div>
 <?php require APPROOT . '/views/common/footer.php'; ?>
