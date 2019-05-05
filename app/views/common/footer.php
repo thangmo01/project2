53fd31e88259a9dfd86c7d261c146b412daf2b9e
60010890 <60010890@kmitl.ac.th>
2019-05-05 14:16:01 +0700

    upload image ui

--- app/views/common/footer.php
+++ app/views/common/footer.php
@@ -1,2 +1,22 @@
+<script>        /* clock */
+    function showTime() 
+    {
+        var date = new Date();
+        var hr = date.getHours();
+        var min = date.getMinutes();
+        var sec = date.getSeconds();   
+        hr = (hr === 0) ? 24 : hr;
+        if (hr > 23) 
+            {   hr = hr - 24;   }
+        hr = (hr < 10) ? "0" + hr : hr;
+        min = (min < 10) ? "0" + min : min;
+        sec = (sec < 10) ? "0" + sec : sec;
+        var time = hr + ":" + min + ":" + sec + " ";
+        var clock = document.querySelector('#clock');
+        clock.innerText = time;
+        setTimeout(showTime, 1000);
+    }
+    showTime();
+</script>
 </body>
 </html>
--- app/views/teacher/index.php
+++ app/views/teacher/index.php
@@ -1,62 +1,38 @@
 <?php require APPROOT . '/views/common/header.php'; ?>
-<li><a href="<?php echo URLROOT . '/teachers/class_detail';?>" class="buttonblue">Detail</a></li>
-<li><a href="<?php echo URLROOT . '/teachers/class_check';?>" class="buttonblue">Check</a></li>
-<div id="clock"></div></ul></div>
-<div style="padding:20px; margin-top:30px;">   
-    <h1>Teacher Page</h1>
+<div class="w3-container">
+    <div class="w3-padding-16" style="display: flex; justify-content: space-between">
+        <h2>Class Total : </h2>
+        <a href="<?php echo URLROOT . '/teachers/class_create';?>" class="buttonblack" style="margin-left:10%; margin-top:8px; margin-bottom:20px;" >Create New Class</a>
+    </div>
+    <div class="w3-responsive">
+        <table class="w3-table-all w3-card-4 w3-small" style="margin-left: auto; margin-right: auto">
+            <tr>
+                <th>Subject ID</th>
+                <th>Subject Name</th>
+                <th>Academic Year</th>
+                <th>Semaster</th>
+                <th>Sec</th>
+                <th>Num Checks</th>
+                <th>Key</th>
+                <th>Detail</th>
+                <th>Check</th>
+            </tr>
+            <?php
+            foreach($data as $sub){
+                echo"<tr>";
+                    echo "<td>".$sub->subject_code."</td>";
+                    echo "<td>".$sub->subject_name."</td>";
+                    echo "<td>".$sub->academic_year."</td>";
+                    echo "<td>".$sub->semaster."</td>";
+                    echo "<td>".$sub->section."</td>";
+                    echo "<td>".$sub->num_checks."</td>";
+                    echo "<td>".$sub->secret."</td>";
+                    echo "<td><a href=' " . URLROOT . "/teachers/classDetail/".$sub->id. "'>detial</a></td>";
+                    echo "<td><a href=' " . URLROOT . "/teachers/classCheck/".$sub->id. "'>check</a></td>";
+                echo"</tr>";
+            }
+            ?>
+        </table>
+    </div>
 </div>
-
-<h2 style="margin-left:10%;">Class Total : </h2>
-<a href="<?php echo URLROOT . '/teachers/class_create';?>" class="buttonblack" style="margin-left:10%; margin-top:8px; margin-bottom:20px;" >Create New Class</a>
-<?php   /*table*/
-//print_r($data);
-    echo"<table>";
-        echo"<tr align = center>"; //head table
-            echo "<th width=10%>Subject ID</th>";
-            echo "<th width=35%>Subject Name</th>";
-            echo "<th width=3%>Academic Year</th>";
-            echo "<th width=3%>Semaster</th>";
-            echo "<th width=2%>Sec</th>";
-            echo "<th width=5%>Num Checks</th>";
-            echo "<th width=37%>Key</th>";
-            echo "<th width=2%>Edit</th>";
-            echo "<th width=3%>Check</th>";
-        echo"</tr>";
-    foreach($data as $sub){
-        echo"<tr align = center>";  //id subject_code  subject_name academic_year semaster section num_checks secret
-            echo "<td>".$sub->subject_code."</td>";
-            echo "<td>".$sub->subject_name."</td>";
-            echo "<td>".$sub->academic_year."</td>";
-            echo "<td>".$sub->semaster."</td>";
-            echo "<td>".$sub->section."</td>";
-            echo "<td>".$sub->num_checks."</td>";
-            echo "<td>".$sub->secret."</td>";
-            echo "<td align=left><a href=' " . URLROOT . "/teachers/classDetail/".$sub->id. "'>edit</a></td>";
-            echo "<td align=left><a href=' " . URLROOT . "/teachers/classCheck/".$sub->id. "'>check</a></td>";
-        echo"</tr>";
-    }
-    echo"</table>";
-?>
-
-<script>        /* clock */
-    function showTime() 
-    {
-        var date = new Date();
-        var hr = date.getHours();
-        var min = date.getMinutes();
-        var sec = date.getSeconds();   
-        hr = (hr === 0) ? 24 : hr;
-        if (hr > 23) 
-            {   hr = hr - 24;   }
-        hr = (hr < 10) ? "0" + hr : hr;
-        min = (min < 10) ? "0" + min : min;
-        sec = (sec < 10) ? "0" + sec : sec;
-        var time = hr + ":" + min + ":" + sec + " ";
-        var clock = document.querySelector('#clock');
-        clock.innerText = time;
-        setTimeout(showTime, 1000);
-    }
-    showTime();
-</script>
-
 <?php require APPROOT . '/views/common/footer.php'; ?>
