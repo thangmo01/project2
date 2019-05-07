15cbfcfcfbd396263ac5c4cccd7ab7fb2f171342
mos_quito@hotmail.com <mos_quito@gmail.com>
2019-05-07 19:26:25 +0700

    responsive index incom

--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -12,14 +12,14 @@ h1 , p{
 }
 </style>
 <div class="coverbgimg">
-  <div class="covermiddle" style="padding:20px 30px 20px 30px;margin-top:10%; margin-left:auto; margin-right:auto;border-radius: 5px; min-width:280px; max-width:70%;background-color: green">
+  <div class="covermiddle" style="padding:15px 20px 15px 20px;margin-top:10%; margin-left:auto; margin-right:auto;border-radius: 5px; min-width:280px; max-width:70%;">
   <div>
     <h1>Project2</h1>
     <hr>
     <p>Project2 is our web project <br>that we made in this year 2019.<br></p>
     <hr>
     <h1><strong> Why it have to be Project2?</strong></h2>
-    <p>Cause Project1 doesn't have "INNOVATION"<br>So it vanished like Thanos snap his finger </p>
+    <p>Cause Project1 doesn't have "INNOVATION"<br>So it vanished like Thanos snap his finger <br><br></p>
   </div>
 </div>
 </div>
