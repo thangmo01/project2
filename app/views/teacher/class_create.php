eb311f3d2350d89e2c72416e50992532faa3eb6a
mos_quito@hotmail.com <mos_quito@gmail.com>
2019-05-07 13:59:47 +0700

    responsive class create

--- app/views/teacher/class_create.php
+++ app/views/teacher/class_create.php
@@ -1,11 +1,11 @@
 <?php require APPROOT . '/views/common/header.php'; ?>
 <?php sessionShowMessage(teacher_create_class) ;?>
 <h2 style="margin-left:25%; margin-top:20px ">Create Class</h2>
-<div style="padding:20px 40px 20px 40px; margin-top:20px; margin-left:25%; margin-right:25%; border-radius: 5px; background-color: #f2f2f2;"> 
+<div style="padding:20px 30px 20px 30px; margin-top:20px; margin: auto; border-radius: 5px; background-color: #f2f2f2; min-width:280px; max-width:55%"> 
   <form action="<?php echo URLROOT . '/teachers/createClass' ?>" method="post">
     <label for="subject_id">Subject</label>
     <div style="white-space: nowrap"> 
-      <select id="subject_id" name="subject_id" style="width:80%">
+      <select id="subject_id" name="subject_id" style="width:70%">
       <?php
         foreach ($data['subject_list'] as $subject) {
           echo '<option value="' . $subject->id . '">' .  $subject->subject_code . ' - ' . $subject->subject_name . '</option>';
@@ -36,7 +36,9 @@
             box-sizing: border-box;">
       </div>
     </div>
-    <input style="margin-left:81%; margin-top:8px;" class="buttonblack" type="submit" value="Create">
+    <div align="right">
+    <input style="margin-right:10%;margin-top:8px;" class="buttonblack" type="submit" value="Create" >
+    </div>
   </form>
 </div>  
 <?php require APPROOT . '/views/common/footer.php'; ?>
