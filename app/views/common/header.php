091a1633ba0f2a124399e88d1a1e980b69f06356
mos_quito@hotmail.com <mos_quito@gmail.com>
2019-05-07 16:56:45 +0700

    header responsive

--- app/views/common/header.php
+++ app/views/common/header.php
@@ -14,21 +14,29 @@
 
     <title><?php echo SITENAME;?></title>
 </head>
+<style>
+    h2{
+        font-size:0.7em;
+    }
+    h3{
+        font-size:1.7rem;
+    }
+</style>
 <body>
 <div class="w3-bar w3-border w3-xlarge w3-dark-grey">
-    <a href="<?php echo URLROOT;?>" class="w3-bar-item w3-button w3-hover-black">Home</a>
+    <a href="<?php echo URLROOT;?>" class="w3-bar-item w3-button w3-hover-black"><h2>Home</h2></a>
     <?php if(isLoggedIn()) : ?>
         <?php if(userType() == 'student'):?>
-            <a href="<?php echo URLROOT . '/students/profile';?>" class="w3-bar-item w3-button w3-hover-black">Profile</a>
-            <a href="<?php echo URLROOT . '/students/joinclass';?>" class="w3-bar-item w3-button w3-hover-black">Joinclass</a>
+            <a href="<?php echo URLROOT . '/students/profile';?>" class="w3-bar-item w3-button w3-hover-black" ><h2>Profile</h2></a>
+            <a href="<?php echo URLROOT . '/students/joinclass';?>" class="w3-bar-item w3-button w3-hover-black" ><h2>Joinclass</h2></a>
         <?php elseif(userType() == 'teacher'):?>
-            <a href="<?php echo URLROOT . '/teachers/createClass';?>" class="w3-bar-item w3-button w3-hover-black">CreateClass</a>
+            <a href="<?php echo URLROOT . '/teachers/createClass';?>" class="w3-bar-item w3-button w3-hover-black" ><h2>CreateClass</h2></a>
         <?php endif;?>
-        <a href="<?php echo URLROOT . '/pages/logout';?>" class="w3-bar-item w3-button w3-right w3-hover-black">Logout</a>
+        <a href="<?php echo URLROOT . '/pages/logout';?>" class="w3-bar-item w3-button w3-right w3-hover-black" ><h2>Logout</h2></a>
     <?php else :
         $authUrl = getGClient()->createAuthUrl();
-        echo '<a href="' . filter_var($authUrl, FILTER_SANITIZE_URL) . '" class="w3-bar-item w3-button w3-right w3-hover-black">Login</a>';
+        echo '<a href="' . filter_var($authUrl, FILTER_SANITIZE_URL) . '" class="w3-bar-item w3-button w3-right w3-hover-black" >Login</a>';
     ?>
     <?php endif; ?>
-    <div id="clock" class="w3-bar-item w3-right"></div>
+    <h3><div id="clock" class="w3-bar-item w3-right"></div></h3>
 </div>
