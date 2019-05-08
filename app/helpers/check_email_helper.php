02db88f3a299c8ff8ae16081d9b9a409990225bf
ThangMo <woradorn.laon@gmail.com>
2019-05-08 14:06:54 +0700

    add email Kaew.ite@gmail.com to student

--- app/helpers/check_email_helper.php
+++ app/helpers/check_email_helper.php
@@ -1,30 +1,32 @@
 <?php
     function isKmitlEmail($email) {
-        $x = preg_match("/^\S+@kmitl.ac.th$/", $email) ||
-            preg_match("/^woradorn.laon@gmail.com$/", $email)  ||
-            preg_match("/^bigguulnw5555@gmail.com$/", $email)  ||
-            preg_match("/^mos43699@gmail.com$/", $email) ||
-            preg_match("/^shuirotsukihoshi@gmail.com$/", $email)
+        $x = preg_match("/^\S+@kmitl.ac.th$/", $email)
+            || preg_match("/^woradorn.laon@gmail.com$/", $email)
+            || preg_match("/^bigguulnw5555@gmail.com$/", $email)
+            || preg_match("/^mos43699@gmail.com$/", $email)
+            || preg_match("/^shuirotsukihoshi@gmail.com$/", $email)
+            || preg_match("/^Kaew.ite@gmail.com$/", $email)
         ;
         return $x;
     }
 
     function type($email) {
         if(
-            preg_match("/^[0-9]{8}@kmitl.ac.th$/", $email) &&
-            !preg_match("/^woradorn.laon@gmail.com$/", $email) &&
-            !preg_match("/^bigguulnw5555@gmail.com$/", $email) &&
-            !preg_match("/^mos43699@gmail.com$/", $email) &&
-            !preg_match("/^shuirotsukihoshi@gmail.com$/", $email)
+            preg_match("/^[0-9]{8}@kmitl.ac.th$/", $email)
+            && !preg_match("/^woradorn.laon@gmail.com$/", $email)
+            && !preg_match("/^bigguulnw5555@gmail.com$/", $email)
+            && !preg_match("/^mos43699@gmail.com$/", $email)
+            && !preg_match("/^shuirotsukihoshi@gmail.com$/", $email)
+            || preg_match("/^Kaew.ite@gmail.com$/", $email)
         ) {
             return 'student';
         }
         elseif(
-            preg_match("/^.+@kmitl.ac.th$/", $email) ||
-            preg_match("/^woradorn.laon@gmail.com$/", $email) ||
-            preg_match("/^bigguulnw5555@gmail.com$/", $email) ||
-            preg_match("/^mos43699@gmail.com$/", $email) ||
-            preg_match("/^shuirotsukihoshi@gmail.com$/", $email)
+            preg_match("/^.+@kmitl.ac.th$/", $email) 
+            || preg_match("/^woradorn.laon@gmail.com$/", $email)
+            || preg_match("/^bigguulnw5555@gmail.com$/", $email)
+            || preg_match("/^mos43699@gmail.com$/", $email)
+            || preg_match("/^shuirotsukihoshi@gmail.com$/", $email)
             ) {
             return 'teacher';
         }
